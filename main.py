80a0b571d82547ec7f429cccc73b127cefe640cf
demonomed85 <politeh@mail.ru>
2024-11-10 13:30:09 +0300

    Init

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
-import time
-import csv
+from selenium.webdriver.firefox.options import Options
 
 url = 'https://www.divan.ru/'
-site = webdriver.Firefox()
+
+options = Options()
+options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0")
+site = webdriver.Firefox(options=options)
+#site = webdriver.Chrome()
 
 try:
     site.get(url)
-    time.sleep(3)
-    print('1')
-    cards = site.find_elements(By.CLASS_NAME, 'lsooF')
-    print(len(cards))
+    print(site)
 
-#    for card in cards:
-#       print(card.text)
 
-except TimeoutException:
-    print("Время ожидания истекло: элементы не были найдены.")
-except NoSuchElementException:
-    print("Элементы не найдены на странице.")
+    try:
+        cards = WebDriverWait(site, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'lsooF')))
+        print(f"Найдено {len(cards)} элементов с классом 'lsooF'")
+
+        for card in cards:
+            print(card.text)
+
+    except TimeoutException:
+        print("Время ожидания истекло: элементы не были найдены.")
+    except NoSuchElementException:
+        print("Элементы не найдены на странице.")
+
 except WebDriverException as e:
     print(f"Ошибка веб-драйвера: {e}")
+
 finally:
     site.quit()
